cdeabf2f6f0cf93343c21d30931905ae9315d935
Amel Muminovic <amcenp@gmail.com>
2020-08-11 17:27:52 +0200

    Updates

---
+++ precache-manifest.2871a6027126e3e7ba7fc5a36d6ccc24.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fe37802d0205873fe6455b07b8d2ed4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b642dc242c374100256",
+    "url": "/static/css/main.d7de0412.chunk.css"
+  },
+  {
+    "revision": "8abc5808a64a54fa0af1",
+    "url": "/static/js/2.2325a0d6.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.2325a0d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b642dc242c374100256",
+    "url": "/static/js/main.d3445d12.chunk.js"
+  },
+  {
+    "revision": "2523d14d68b532344f87",
+    "url": "/static/js/runtime-main.3af121d5.js"
+  }
+]);
